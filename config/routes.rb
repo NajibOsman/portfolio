d5a9a5ca594777cf2080037924e555aa0d1d8724
NajibOsman <machidydemajka38@gmail.com>
2017-08-14 22:23:50 +0300

    Root routed and migrated the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'post/index'
+
+  devise_for :users
+  get 'blog/index'
+
   get 'skills/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'home#index'
   resources :projects
   resources :skills
+  resources :blogs
+
 end
