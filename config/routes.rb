a195d2f1955cb5ba7bfb37feddc70a19d5d10103
NajibOsman <machidydemajka38@gmail.com>
2017-08-17 11:02:57 +0300

    routes have been rendered

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'post/index'
 
-  devise_for :users
-  get 'blog/index'
-
-  get 'skills/index'
-
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'home#index'
   resources :projects
   resources :skills
-  resources :blogs
 
+  resources :posts do
+    resources :comments
+  end
+  resources :users
+  devise_for :users
 end
